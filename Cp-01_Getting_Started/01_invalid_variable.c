7da7e8c347a9cd555ba357a529f290ee077a4283
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2025-09-28 14:59:23 +0200

    Add Chapter 1 files

---
+++ Cp-01_Getting_Started/01_invalid_variable.c
@@ -0,0 +1,20 @@
+/*
+[A] Which of the following are invalid variable names and why?
+BASICSALARY  |  _basic  |  basic-hra
+#MEAN        |  group.  |  422
+FLOAT        |  hELLO   | queue.
+team’svictory| Plot # 3 | 2015_DDay
+population in 2006 | over time | mindovermatter
+
+
+<--------- ANSWER --------->
+1) basic-hra
+2) #MEAN
+3) group.
+4) 422
+5) queue.
+6) team’svictory
+7) Plot # 3
+8) population in 2006
+9) over time
+*/
