65742426890dc2dbe6eb8432f9698009b53440ba
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2025-10-01 15:13:38 +0200

    Solve more question on 07_File

--- Cp-01_Getting_Started/07_write_program.c
+++ Cp-01_Getting_Started/07_write_program.c
@@ -11,13 +11,24 @@ subject is 100.
 
 (d) Temperature of a city in Fahrenheit degrees is input through the keyboard. Write a program to convert this temperature into Centigrade degrees.
 
-(e) The length & breadth of a rectangle and radius of a circle are input through the keyboard. Write a program to calculate the area & perimeter of the rectangle, and the area & circumference of the circle. 
+(e) The length & breadth of a rectangle and radius of a circle are input through the keyboard. Write a program to calculate the area & perimeter of the rectangle, and the area & circumference of the circle.
+
+(f) Two numbers are input through the keyboard into two locations C and D. Write a program to interchange the contents of C and D.
+
+(g) If a five-digit number is input through the keyboard, write a program to calculate the sum of its digits.
+
+(i) If a four-digit number is input through the keyboard, write a program to obtain the sum of the first and last digit of this number.
+
+(j) In a town, the percentage of men is 52. The percentage of total literacy is 48. If total percentage of literate men is 35 of the total population, write a program to find the total number of illiterate men and women if the population of the town is 80,000.
+
+(k) A cashier has currency notes of denominations 10, 50 and 100. If the amount to be withdrawn is input through the keyboard in hundreds, find the total number of currency notes of each denomination the cashier will have to give to the withdrawer.
 
 */
 
 #include <stdio.h>
 
 int main() {
+
     /*
     <_______ Code for a ... _______>
         float salary;
@@ -90,6 +101,118 @@ int main() {
         printf("The Area of a Rectangle: %d\nThe Perimeter of a Rectangle: %d\n", area_rect, perimeter_rect);
         printf("The Area of a Circle: %.2f\nThe Circumference of a Circle: %.2f\n", area_circle, circumference_circle);
     */
+
+    /*
+    <_______ Code for f ... _______>
+        int c, d, temp;
+        printf("Enter the number for C & D\n");
+        scanf("%d %d", &c, &d);
+
+        printf("C: %d\tD: %d\n", c, d);
+
+        temp = c;
+        c = d;
+        d = temp;
+
+        printf("C: %d\tD: %d\n", c, d);
+    */
+
+    /*
+    <_______ Code for g ... _______>
+        int num, sum_no = 0;
+        printf("Enter 5 numbers:\n");
+
+        for (int i = 0; i < 5; i++) {
+            printf("%d -> ", i + 1);
+            scanf("%d", &num);
+            sum_no += num;
+        }
+
+        printf("The total sum of the numbers are %d\n", sum_no);
+    */
+
+    /*
+    <_______ Code for h ... _______>
+        int n, reminder, reversed_no = 0;
+        printf("Enter the number to reverse it - ");
+        scanf("%d", &n);
+
+        while (n != 0) {
+            reminder = n % 10;
+            reversed_no = (reversed_no * 10) + reminder;
+            n = n / 10;
+        }
+
+        printf("Reversed Number - %d", reversed_no);
+    */
+
+    /*
+    <_______ Code for i ... _______>
+        int number, last_num, first_num, sum_num;
+        printf("Enter four digit number: ");
+        scanf("%d", &number);
+
+        last_num = number % 10;
+
+        while (number > 0) {
+            first_num = number % 10;
+            number = number / 10;
+        }
+
+        sum_num = first_num + last_num;
+
+        printf("Sum of First no [%d] and Last no [%d] : %d", first_num, last_num, sum_num);
+    */
+
+    /*
+    <_______ Code for j ... _______>
+        float
+            total_population,
+            total_literacy,
+            men, women,
+            literate_men,
+            literate_women,
+            illiterate_men,
+            illiterate_women;
+
+        total_population = 80000.00;
+        men = 0.52 * 80000.00;
+        women = 0.48 * 80000.00;
+
+        total_literacy = 0.48 * 80000.00;  // (men + women who can read/write)
+        literate_men = 0.35 * 80000.00;
+
+        // literate_women (Find)
+        // illiterate_men (Find)
+        // illiterate_women (Find)
+
+        literate_women = total_literacy - literate_men;
+        illiterate_men = men - literate_men;
+        illiterate_women = women - literate_women;
+
+        printf("Illiterate Men: %.2f\n", illiterate_men);
+        printf("Illiterate Women: %.2f\n", illiterate_women);
+    */
+
+    // <_______ Code for k ... _______>
+    int withdraw;
+    printf("Enter the amount to withdraw: ");
+    scanf("%d", &withdraw);
+
+    int hundreds, fiftys, tens;
+    int remaining_after_100s, remaining_after_50s;
+
+    hundreds = withdraw / 100;
+    remaining_after_100s = withdraw % 100;
+
+    fiftys = remaining_after_100s / 50;
+    remaining_after_50s = remaining_after_100s % 50;
+
+    tens = remaining_after_50s / 10;
+
+    printf("100s: %d\n50s: %d\n10s: %d\n", hundreds, fiftys, tens);
+
+
 
     return 0;
 }
