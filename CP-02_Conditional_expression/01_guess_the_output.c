ac76dbeea6cd294989190a0ba028bf30435d16dd
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2025-10-05 16:03:41 +0200

    Finish Chapter 01 Practice Question.

---
+++ CP-02_Conditional_expression/01_guess_the_output.c
@@ -0,0 +1,11 @@
+/*
+[A] What would be the output of the following programs:
+
+(a) main( ) {
+        int a = 300, b, c ;
+        if ( a >= 400 )
+        b = 300 ;
+        c = 200 ;
+        printf ( "\n%d %d", b, c ) ;
+    } 
+*/
--- Cp-01_Getting_Started/07_write_program.c
+++ Cp-01_Getting_Started/07_write_program.c
@@ -23,176 +23,159 @@ subject is 100.
 
 (k) A cashier has currency notes of denominations 10, 50 and 100. If the amount to be withdrawn is input through the keyboard in hundreds, find the total number of currency notes of each denomination the cashier will have to give to the withdrawer.
 
+(l) If the total selling price of 15 items and the total profit earned on them is input through the keyboard, write a program to find the cost price of one item. 
+
+(m) If a five-digit number is input through the keyboard, write a program to print a new number by adding one to each of its digits. For example if the number that is input is 12391 then the output should be displayed as 23402.
 */
 
 #include <stdio.h>
 
 int main() {
 
-    /*
-    <_______ Code for a ... _______>
-        float salary;
-        printf("Enter Salary:\n");
-        scanf("%f", &salary);
+    // <_______ Code for a ... _______>
+    float salary;
+    printf("Enter Salary:\n");
+    scanf("%f", &salary);
 
-        float dearness = 0.4 * salary;
-        float rent = 0.2 * salary;
+    float dearness = 0.4 * salary;
+    float rent = 0.2 * salary;
 
-        printf("Ramesh's gross salary is: %.2f", salary + dearness + rent);
-    */
+    printf("Ramesh's gross salary is: %.2f", salary + dearness + rent);
+    
+    // <_______ Code for b ... _______>
+    float distance;
+    printf("Enter Distance:\n");
+    scanf("%f", &distance);
 
-    /*
-    <_______ Code for b ... _______>
-        float distance;
-        printf("Enter Distance:\n");
-        scanf("%f", &distance);
+    float meters = distance * 1000;
+    float feet = meters * 3.28084;
+    float inches = feet * 12;
+    float centimeters = distance * 100000;
 
-        float meters = distance * 1000;
-        float feet = meters * 3.28084;
-        float inches = feet * 12;
-        float centimeters = distance * 100000;
+    printf("Your Distance in Meters, Feet, Inches, Centimeters:\n");
+    printf("Meters: %.2f m\nFeet: %.2f f\nInches: %.2f i\nCentimeters: %.2f cm\n", meters, feet, inches, centimeters);
 
-        printf("Your Distance in Meters, Feet, Inches, Centimeters:\n");
-        printf("Meters: %.2f m\nFeet: %.2f f\nInches: %.2f i\nCentimeters: %.2f cm\n", meters, feet, inches, centimeters);
-    */
+    // <_______ Code for c ... _______>
+    int marks, aggregate_marks = 0;
+    float percentage;
 
-    /*
-    <_______ Code for c ... _______>
-        int marks, aggregate_marks = 0;
-        float percentage;
+    for (int i = 0; i < 5; i++) {
+        printf("Give the marks of %d subject: ", i + 1);
+        scanf("%d", &marks);
 
-        for (int i = 0; i < 5; i++) {
-            printf("Give the marks of %d subject: ", i + 1);
-            scanf("%d", &marks);
+        aggregate_marks += marks;
+    }
 
-            aggregate_marks += marks;
-        }
+    percentage = (aggregate_marks / 500.0) * 100.0;
 
-        percentage = (aggregate_marks / 500.0) * 100.0;
+    printf("The Aggregate Marks: %d\n", aggregate_marks);
+    printf("The Percentage Marks: %.2f\n", percentage);
 
-        printf("The Aggregate Marks: %d\n", aggregate_marks);
-        printf("The Percentage Marks: %.2f\n", percentage);
-    */
+    // <_______ Code for d ... _______>
+    float fahrenheit;
+    printf("Enter Fahrenheit to convert it into Celsius: ");
+    scanf("%f", &fahrenheit);
 
-    /*
-    <_______ Code for d ... _______>
-        float fahrenheit;
-        printf("Enter Fahrenheit to convert it into Celsius: ");
-        scanf("%f", &fahrenheit);
+    float celsius = ((5.0 / 9.0) * (fahrenheit - 32));
 
-        float celsius = ((5.0 / 9.0) * (fahrenheit - 32));
+    printf("The Celsius is: %.2f", celsius);
 
-        printf("The Celsius is: %.2f", celsius);
-    */
+    // <_______ Code for e ... _______>
+    int length, breath;
+    float radius;
+    printf("Enter: 1)Length  2)Breath  3)Raduis\n");
+    scanf("%d %d %f", &length, &breath, &radius);
 
-    /*
-    <_______ Code for e ... _______>
-        int length, breath;
-        float radius;
-        printf("Enter: 1)Length  2)Breath  3)Raduis\n");
-        scanf("%d %d %f", &length, &breath, &radius);
+    int area_rect = length * breath;
+    int perimeter_rect = 2 * (length * breath);
 
-        int area_rect = length * breath;
-        int perimeter_rect = 2 * (length * breath);
+    float area_circle = 3.14 * (radius * radius);
+    float circumference_circle = 2 * 3.14 * radius;
 
-        float area_circle = 3.14 * (radius * radius);
-        float circumference_circle = 2 * 3.14 * radius;
+    printf("The Area of a Rectangle: %d\nThe Perimeter of a Rectangle: %d\n", area_rect, perimeter_rect);
+    printf("The Area of a Circle: %.2f\nThe Circumference of a Circle: %.2f\n", area_circle, circumference_circle);
 
-        printf("The Area of a Rectangle: %d\nThe Perimeter of a Rectangle: %d\n", area_rect, perimeter_rect);
-        printf("The Area of a Circle: %.2f\nThe Circumference of a Circle: %.2f\n", area_circle, circumference_circle);
-    */
+    // <_______ Code for f ... _______>
+    int c, d, temp;
+    printf("Enter the number for C & D\n");
+    scanf("%d %d", &c, &d);
 
-    /*
-    <_______ Code for f ... _______>
-        int c, d, temp;
-        printf("Enter the number for C & D\n");
-        scanf("%d %d", &c, &d);
+    printf("C: %d\tD: %d\n", c, d);
 
-        printf("C: %d\tD: %d\n", c, d);
+    temp = c;
+    c = d;
+    d = temp;
 
-        temp = c;
-        c = d;
-        d = temp;
+    printf("C: %d\tD: %d\n", c, d);
 
-        printf("C: %d\tD: %d\n", c, d);
-    */
+    // <_______ Code for g ... _______>
+    int num, sum_no = 0;
+    printf("Enter 5 numbers:\n");
 
-    /*
-    <_______ Code for g ... _______>
-        int num, sum_no = 0;
-        printf("Enter 5 numbers:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d -> ", i + 1);
+        scanf("%d", &num);
+        sum_no += num;
+    }
 
-        for (int i = 0; i < 5; i++) {
-            printf("%d -> ", i + 1);
-            scanf("%d", &num);
-            sum_no += num;
-        }
+    printf("The total sum of the numbers are %d\n", sum_no);
 
-        printf("The total sum of the numbers are %d\n", sum_no);
-    */
+    // <_______ Code for h ... _______>
+    int n, reminder, reversed_no = 0;
+    printf("Enter the number to reverse it - ");
+    scanf("%d", &n);
 
-    /*
-    <_______ Code for h ... _______>
-        int n, reminder, reversed_no = 0;
-        printf("Enter the number to reverse it - ");
-        scanf("%d", &n);
+    while (n != 0) {
+        reminder = n % 10;
+        reversed_no = (reversed_no * 10) + reminder;
+        n = n / 10;
+    }
 
-        while (n != 0) {
-            reminder = n % 10;
-            reversed_no = (reversed_no * 10) + reminder;
-            n = n / 10;
-        }
+    printf("Reversed Number - %d", reversed_no);
 
-        printf("Reversed Number - %d", reversed_no);
-    */
+    // <_______ Code for i ... _______>
+    int number, last_num, first_num, sum_num;
+    printf("Enter four digit number: ");
+    scanf("%d", &number);
 
-    /*
-    <_______ Code for i ... _______>
-        int number, last_num, first_num, sum_num;
-        printf("Enter four digit number: ");
-        scanf("%d", &number);
+    last_num = number % 10;
 
-        last_num = number % 10;
+    while (number > 0) {
+        first_num = number % 10;
+        number = number / 10;
+    }
 
-        while (number > 0) {
-            first_num = number % 10;
-            number = number / 10;
-        }
+    sum_num = first_num + last_num;
 
-        sum_num = first_num + last_num;
+    printf("Sum of First no [%d] and Last no [%d] : %d", first_num, last_num, sum_num);
 
-        printf("Sum of First no [%d] and Last no [%d] : %d", first_num, last_num, sum_num);
-    */
+    // <_______ Code for j ... _______>
+    float
+        total_population,
+        total_literacy,
+        men, women,
+        literate_men,
+        literate_women,
+        illiterate_men,
+        illiterate_women;
 
-    /*
-    <_______ Code for j ... _______>
-        float
-            total_population,
-            total_literacy,
-            men, women,
-            literate_men,
-            literate_women,
-            illiterate_men,
-            illiterate_women;
+    total_population = 80000.00;
+    men = 0.52 * 80000.00;
+    women = 0.48 * 80000.00;
 
-        total_population = 80000.00;
-        men = 0.52 * 80000.00;
-        women = 0.48 * 80000.00;
+    total_literacy = 0.48 * 80000.00;  // (men + women who can read/write)
+    literate_men = 0.35 * 80000.00;
 
-        total_literacy = 0.48 * 80000.00;  // (men + women who can read/write)
-        literate_men = 0.35 * 80000.00;
+    // literate_women (Find)
+    // illiterate_men (Find)
+    // illiterate_women (Find)
 
-        // literate_women (Find)
-        // illiterate_men (Find)
-        // illiterate_women (Find)
+    literate_women = total_literacy - literate_men;
+    illiterate_men = men - literate_men;
+    illiterate_women = women - literate_women;
 
-        literate_women = total_literacy - literate_men;
-        illiterate_men = men - literate_men;
-        illiterate_women = women - literate_women;
-
-        printf("Illiterate Men: %.2f\n", illiterate_men);
-        printf("Illiterate Women: %.2f\n", illiterate_women);
-    */
+    printf("Illiterate Men: %.2f\n", illiterate_men);
+    printf("Illiterate Women: %.2f\n", illiterate_women);
 
     // <_______ Code for k ... _______>
     int withdraw;
@@ -211,6 +194,17 @@ int main() {
     tens = remaining_after_50s / 10;
 
     printf("100s: %d\n50s: %d\n10s: %d\n", hundreds, fiftys, tens);
+
+    // <_______ Code for l ... _______>
+    int sellingPrice, profit;
+    printf("Enter the selling price of 15 items: ");
+    scanf("%d", &sellingPrice);
+    printf("Enter only the total profit: ");
+    scanf("%d", &profit);
+
+    float costPrice = (sellingPrice - profit) / 15.0;
+
+    printf("Cost price of one item: %.2f", costPrice);
 
     return 0;
 }
