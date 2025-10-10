916768f55cb56392065085a0a161fbd9399fa6e8
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2025-10-10 15:31:51 +0200

    Complete all questions in File 05 of Chapter 02.

---
+++ CP-02_Conditional_expression/05_point_more_error.c
@@ -0,0 +1,72 @@
+/*
+[E] Point out the errors, if any, in the following programs:
+
+(a) /* This program
+    /* is an example of
+    /* using Logical operators *\/
+    main( ) {
+        int i = 2, j = 5 ;
+        if ( i == 2 && j == 5 )
+        printf ( "\nSatisfied at last" ) ;
+    }
+
+(b) main( ) {
+        int code, flag ;
+        if ( code == 1 & flag == 0 )
+            printf ( "\nThe eagle has landed" ) ;
+    }
+
+(c) main( ) {
+        char spy = 'a', password = 'z' ;
+        if ( spy == 'a' or password == 'z' )
+        printf ( "\nAll the birds are safe in the nest" ) ;
+    }
+
+(d) main( ) {
+        int i = 10, j = 20 ;
+        if ( i = 5 ) && if ( j = 10 )
+        printf ( "\nHave a nice day" ) ;
+    }
+
+(e) main( ) {
+        int x = 10 , y = 20;
+        if ( x >= 2 and y <=50 )
+        printf ( "\n%d", x ) ;
+    }
+
+(f) main( ) {
+        int a, b ;
+        if ( a == 1 & b == 0 )
+        printf ( "\nGod is Great" ) ;
+    }
+
+(g) main( ) {
+        int x = 2;
+        if ( x == 2 && x != 0 ) ;
+        {
+            printf ( "\nHi" ) ;
+            printf( "\nHello" ) ;
+        }
+        else
+            printf( "Bye" ) ;
+    }
+
+(h) main( ) {
+        int i = 10, j = 10 ;
+        if ( i && j == 10)
+            printf ( "\nHave a nice day" ) ;
+    }
+
+*/
+
+#include<stdio.h>
+
+int main() {
+
+    printf("There is an error in problem (c). \n");
+    printf("There is an error in problem (d). \n");
+    printf("There is an error in problem (e). \n");
+    printf("There is an error in problem (f). \n");    
+
+    return 0;
+}
